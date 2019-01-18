38461015ef8fdd92faa26e8e6f4ba067c4accddb
Nick Ulricksen <n.ulricksen@gmail.com>
2019-01-18 15:38:58 -0800

    set up renderToNodeStream to stream the document in 3 parts, split by the react app

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import { renderToNodeStream } from 'react-dom/server';
 import { ServerLocation } from '@reach/router';
 import fs from 'fs';
 import App from './src/App';
@@ -14,14 +14,24 @@ const parts = html.split('Requires javascript!');
 
 app.use('/dist', express.static('dist'));
 app.use((req, res) => {
+  res.write(parts[0]);
   const reactMarkup = (
     <ServerLocation url={req.url}>
       <App />
     </ServerLocation>
   );
 
-  res.send(`${parts[0]}${renderToString(reactMarkup)}${parts[1]}`);
-  res.end();
+  const stream = renderToNodeStream(reactMarkup);
+
+  stream.pipe(
+    res,
+    { end: false }
+  );
+
+  stream.on('end', () => {
+    res.write(parts[1]);
+    res.end();
+  });
 });
 
 app.listen(PORT, () => {
